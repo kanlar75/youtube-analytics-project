1dc41a64206e98036e3fb81a998fbf1792a7490c
kanlar75 <kanlar75@gmail.com>
2023-05-29 14:13:36 +0300

    создан класс Channel и метод печати инфо

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,28 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+api_key = os.getenv('YOU_TUBE_API_KEY')
+
+# создаем специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        """Экземпляр инициализируется id канала. Дальше все данные будут
+        подтягиваться по API."""
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        data = youtube.channels().list(id=self.channel_id,
+                                          part='snippet,statistics').execute()
+        print(json.dumps(data, indent=2, ensure_ascii=False))
+
+
+
+
