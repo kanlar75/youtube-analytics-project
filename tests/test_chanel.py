234a18e7f83c7e16e91c435c807c62a10e556915
kanlar75 <kanlar75@gmail.com>
2023-05-30 10:43:50 +0300

    добавлены тесты class Chanel

---
+++ tests/test_chanel.py
@@ -0,0 +1,17 @@
+import contextlib
+import io
+
+from src.channel import Channel
+
+
+def test_init(obj_chanel):
+    assert isinstance(obj_chanel, Channel)
+    obj1 = Channel("test")
+    assert obj1.channel_id == "test"
+
+
+def test_print_info(obj_chanel, test_data):
+    s = io.StringIO()
+    with contextlib.redirect_stdout(s):
+        obj_chanel.print_info()
+    assert s.getvalue() == f'{test_data}"\n"'
