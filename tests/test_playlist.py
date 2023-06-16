31127cde826888334555153488a1da8668f2a6d6
kanlar75 <kanlar75@gmail.com>
2023-06-16 07:08:52 +0300

    комментарии

--- tests/test_playlist.py
+++ tests/test_playlist.py
@@ -1,12 +1,16 @@
 import datetime
 
 
+# Тестируем инициализацию класса PlayList
+
 def test_init(test_obj5):
     assert test_obj5.title == "Moscow Python Meetup №81"
     assert test_obj5.url == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj" \
-                    "-n2qLkEM2Hj96LO6uqgQw"
+                            "-n2qLkEM2Hj96LO6uqgQw"
 
 
+# Тестируем принадлежность к классу `datetime.timedelta`, суммарную
+# длительность плейлиста, строковое представление длительности
 def test_total_duration(test_obj5):
     duration = test_obj5.total_duration
     assert str(duration) == "1:49:52"
@@ -14,5 +18,7 @@ def test_total_duration(test_obj5):
     assert duration.total_seconds() == 6592.0
 
 
+# Тестируем возвращение строки на самое популярное видео из плейлиста (по
+# количеству лайков)
 def test_show_best_video(test_obj5):
     assert test_obj5.show_best_video() == "https://youtube/cUGyMzWQcGM"
