a92b483d93f39e59be7e06a9227edecd9cb6dbe0
kanlar75 <kanlar75@gmail.com>
2023-06-17 06:45:31 +0300

    док стринги

--- src/playlist.py
+++ src/playlist.py
@@ -4,16 +4,19 @@ import isodate
 from googleapiclient.discovery import build
 from src.channel import api_key
 
-from src.video import PLVideo, Video
+from src.video import Video
 
 
 class MixVideo:
+
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, id_playlist):
         self.id_playlist = id_playlist
 
     def get_video(self):
+        """ Получаем данные по видеороликам в плейлисте. """
+
         video = self.youtube.playlistItems().list(playlistId=self.id_playlist,
                                                   part='contentDetails',
                                                   maxResults=50,
@@ -21,12 +24,14 @@ class MixVideo:
         return video
 
     def get_video_id(self):
+        """ Получаем все id видеороликов из плейлиста. """
+
         video_ids: list[str] = [video['contentDetails']['videoId'] for video in
                                 self.get_video()['items']]
         return video_ids
 
 
-class PlayList(MixVideo, Video):
+class PlayList(MixVideo):
 
     def __init__(self, id_playlist):
         super().__init__(id_playlist)
@@ -34,18 +39,17 @@ class PlayList(MixVideo, Video):
         self.title = self.data['items'][0]['snippet']['title']
         self.url = f'https://www.youtube.com/playlist?list={id_playlist}'
 
-    def __str__(self):
-
-        return f"{self.total_duration.str('%H:%M:%S')}"
-
-    def get_data(self, playlist):
+    def get_data(self, id_playlist):
         """ Получает данные от YouTube. """
 
-        self.data = MixVideo.youtube.playlists().list(part='snippet',
-                                                      id=playlist).execute()
+        self.data = self.youtube.playlists().list(part='snippet',
+                                                      id=id_playlist).execute()
         return self.data
 
     def show_best_video(self):
+        """ Возвращаем ссылку на самое популярное видео из плейлиста (по
+        количеству лайков). """
+
         lst_video = []
         for video in self.get_video_id():
             v = Video(video)
@@ -55,6 +59,8 @@ class PlayList(MixVideo, Video):
 
     @property
     def total_duration(self):
+        """ Возвращаем объект класса `datetime.timedelta` с суммарной
+        длительность плейлиста. """
 
         video_response = self.youtube.videos().list(
             part='contentDetails,statistics',
